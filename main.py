6ecc6a3fb9cd0d732089a9d087af1049f987da21
fourn99 <nic_fou@hotmail.com>
2019-10-18 14:55:52 -0400

    initial commit part 3

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 
+from question_1 import *
+#from question_2 import *
+
+
+def main():
+    
+
+     print("Assignment 1 - Question 1")
+     answer_number_one()
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
